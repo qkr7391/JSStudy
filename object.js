f0ce23cd6df2d5f51abc89372f0110a7311fc936
Sammy Park <saemipark7391@gmail.com>
2023-08-23 16:51:28 -0400

    object

---
+++ object.js
@@ -0,0 +1,29 @@
+let applename = 'apple';
+let applecolor = 'red';
+let appledisplay = '🍎'
+
+let orangeName = 'orange';
+
+// ^ need to grouping/packaging
+
+let apple = {
+    name : 'apple',
+    color : 'red',
+    display : '🍎'
+}
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+let orange = {
+    name : 'orange',
+    color : 'orange',
+    display : '🍊'
+}
+
+console.log(orange);
+console.log(orange.name);
+console.log(orange.color);
+console.log(orange.display);
