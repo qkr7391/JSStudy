e1c248d1cc09bec5d7d247e3a0dd399b92467453
Sammy Park <saemipark7391@gmail.com>
2023-08-23 18:28:13 -0400

    ternary

---
+++ ternary.js
@@ -0,0 +1,9 @@
+//ternary operator
+// condition ? true : false
+
+let fruit = 'apple';
+
+fruit === 'apple' ? console.log('🍎') : console.log(😒);
+
+let emoji = fruit === 'apple' ? console.log('🍎') : console.log(😒);
+console.log(emoji);
