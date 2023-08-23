9cef71b740ed6fb40e594891414431473675afc1
Sammy Park <saemipark7391@gmail.com>
2023-08-23 16:43:17 -0400

    js study

---
+++ boolean.js
@@ -0,0 +1,30 @@
+let correct = true;
+let incorrect = false;
+console.log(correct);
+console.log(incorrect);
+
+
+//ex
+let isFree = true;
+let isActivated = false;
+let isEnrolled = true;
+console.log(isFree);
+
+
+console.log('*********************************');
+
+//falshy !!-> true or false
+console.log(!!0);
+console.log(!!-0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+
+console.log('****');
+//truthy
+console.log(!!1);
+console.log(!!-1);
+console.log(!!'yes');
+console.log(!!{});
+console.log(!!Infinity);
---
+++ number.js
@@ -0,0 +1,23 @@
+let integer = 123;
+let negative = -123;
+let double = 1.23;
+console.log(integer);
+console.log(negative);
+console.log(double);
+
+let binary = 0b1111011;
+console.log(binary);
+let octal = 0o173;
+console.log(octal);
+let hex = 0x7b;
+console.log(hex);
+
+
+console.log(0/123);
+console.log(123/0); //Infinity
+console.log(123/-0); //-Infinity
+console.log(123/'text'); //NaN == Not a Number
+
+
+let bigInt = 1232547568456948537456234534534523456476587987n;
+console.log(bigInt);
