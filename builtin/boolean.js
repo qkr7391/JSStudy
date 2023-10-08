35d0bf07ba5979f6c0dcfcb83157f2f03c24f251
Saemi Park <saemipark7391@gmail.com>
2023-10-08 12:35:32 -0400

    boolean object

---
+++ builtin/boolean.js
@@ -0,0 +1,26 @@
+//Boolean object
+true.valueOf;
+
+const isTrue = true;
+// const isTrue2 = new Boolean(true); //using object use lots of memory
+console.log(isTrue.valueOf());
+
+/**
+ * Falshy
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ * ''
+ */
+
+/*
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ */
