7da93eb046b5c7695ea3ffe284bf4f5ab78bb26e
Saemi Park <saemipark7391@gmail.com>
2023-10-08 14:31:59 -0400

    Math

---
+++ builtin/math.js
@@ -0,0 +1,26 @@
+// Math - static properties, method
+
+console.log(Math.E); // Euler's Constants
+console.log(Math.PI);
+
+// static method
+console.log(Math.abs(-10)); // Absolute value
+console.log(Math.ceil(1.4)); // raised
+console.log(Math.floor(1.4)); // down
+console.log(Math.round(1.4)); //round
+console.log(Math.round(1.7)); //round
+console.log(Math.trunc(1.532)); // throw numbers under decimal
+
+console.log(Math.max(1, 2, 3)); //max value
+console.log(Math.min(1, 2, 3)); //min value
+
+console.log(3 ** 2); // Powers
+console.log(Math.pow(3, 2)); //Powers
+
+console.log(Math.sqrt(9)); //Square Root
+
+console.log(Math.random()); // between 0 - 1
+
+// what if 1 - 10
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10 + 1));
