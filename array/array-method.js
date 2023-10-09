4bd53edb37ba68c7efa32d8add5eb2637f1e7b43
Saemi Park <saemipark7391@gmail.com>
2023-10-09 14:49:17 -0400

    array-methods

--- array/array-method.js
+++ array/array-method.js
@@ -38,3 +38,54 @@ console.log(fruits);
 console.log(deleted); // array state deleted item is returned
 fruits.splice(1, 0, "🍓", "🫐"); // from 1 (index), delete 0, add  "🍓", "🫐"
 console.log(fruits);
+
+//make new array || return new array
+let newArr = fruits.slice(0, 2); //cut and copy array from o(index) to before 2(index) in fruits array to newArr
+console.log(newArr); // copied
+console.log(fruits); // nothing changed
+
+newArr = fruits.slice();
+console.log(newArr);
+
+newArr = fruits.slice(-1);
+console.log(newArr);
+
+//concate array and array2 ...
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+const arr4 = arr3.reverse(); //copy reversed array
+console.log(arr4);
+
+console.clear();
+
+let arr = [
+	[1, 2, 3],
+	[4, [5, 6]],
+];
+console.log(arr);
+console.log(arr.flat()); // falt 1 stage array
+console.log(arr.flat(2)); // flat 2 stage array
+//flat did not change origin array. return new array.
+
+arr = arr.flat(2);
+arr.fill(0); // fill array with 0 || change origin array
+console.log(arr);
+
+arr.fill("s", 1, 3); //fill 's' from 1 (index) to before 3 (index)
+console.log(arr);
+
+arr.fill("s", 1); // fill 's' from 1 (index) to end
+console.log(arr);
+
+// items in array changed to string and combined
+let txt = arr.join(); //basic using ','
+console.log(txt);
+
+txt = arr.join("|"); //using |
+console.log(txt);
+//join return string value, not change origin array
