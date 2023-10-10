49d60f60204cc1c164e300e9d113a4a4f1d60de9
Saemi Park <saemipark7391@gmail.com>
2023-10-10 16:44:01 -0400

    comments information

---
+++ comments.js
@@ -0,0 +1,19 @@
+// comments >> one line only
+
+//TODO(Leo) : Make function makeNumber();
+
+/**
+ *  comments is not for explain code
+ * comments used for explain WHY and HOW
+ */
+
+//JSDoc _ ex
+/**
+ * return sum of two parameters
+ * @param {*} a num 1
+ * @param {*} b num 2
+ * @returns  value of a + b
+ */
+function add(a, b) {
+	return a + b;
+}
