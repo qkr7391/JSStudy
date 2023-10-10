aeeb52eb41bcb6a0b9c20af36cc7b780b64bed30
Saemi Park <saemipark7391@gmail.com>
2023-10-10 16:37:35 -0400

    nullish

---
+++ nullish.js
@@ -0,0 +1,8 @@
+//Nullish Coalescing Operator
+//??
+// || when falshy --> assign
+let num = 0;
+let num2; //undefined
+console.log(num || "-1"); //-1
+console.log(num ?? "-1"); //0
+console.log(num2 ?? "-1"); //-1
